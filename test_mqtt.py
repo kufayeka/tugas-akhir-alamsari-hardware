3a28da0c6d2633b71fa0d18044e00d7476338cf5
kufayeka <yayankufa18@gmail.com>
2023-05-20 23:43:53 +0700

    progress: add mqtt connection

--- test_mqtt.py
+++ test_mqtt.py
@@ -1,47 +1,23 @@
-import paho.mqtt.client as mqtt
-
-# MQTT broker details
-host = '203.189.120.137'
-port = 1883
-username = 'petra_mqtt_broker'
-password = '1n!_mqttBroker'
-
-# Callback function for MQTT client connection
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print('Connected to MQTT broker')
-        # Subscribe to a topic upon successful connection
-        client.subscribe('your/topic')  # Replace with your desired topic
-    else:
-        print('Connection failed')
-
-# Callback function for MQTT client disconnection
-def on_disconnect(client, userdata, rc):
-    print('Disconnected from MQTT broker')
-    
-
-# Callback function for MQTT message reception
-def on_message(client, userdata, msg):
-    print('Received message:')
-    print('Topic: ' + msg.topic)
-    print('Message: ' + msg.payload.decode())
-
-# Create MQTT client and set the callback functions
-client = mqtt.Client("baddestbitch")
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
-
-# Set username and password
-client.username_pw_set(username, password)
-
-# Connect to the MQTT broker
-client.connect(host, port)
-
-# Start the MQTT network loop
-client.loop_start()
-
-# Publish a sample message
-client.publish('your/topic', 'Hello, MQTT!')  # Replace with your desired topic and message
-
-# Continue other tasks or wait for messages
+import paho.mqtt.client as mqtt #import the client1
+import time
+############
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+########################################
+broker_address="192.168.43.38"
+#broker_address="broker.hivemq.com"
+print("creating new instance")
+client = mqtt.Client("P1") #create new instance
+client.on_message=on_message #attach function to callback
+print("connecting to broker")
+client.connect(broker_address, port=1883) #connect to broker
+client.loop_start() #start the loop
+print("Subscribing to topic","house/bulbs/bulb1")
+client.subscribe("house/bulbs/bulb1")
+print("Publishing message to topic","house/bulbs/bulb1")
+client.publish("house/bulbs/bulb1","OFF")
+time.sleep(4) # wait
+client.loop_stop() #stop the loop
