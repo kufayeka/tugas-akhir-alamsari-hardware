b7225d2e03d1f6fee8a7ab57a0c4f0060e64fcca
kufayeka <yayankufa18@gmail.com>
2023-05-20 23:52:24 +0700

    progress: fixing the mqtt connect script

--- test_mqtt.py
+++ test_mqtt.py
@@ -1,23 +1,42 @@
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt
 import time
-############
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")
+        client.subscribe("house/bulbs/bulb1")
+    else:
+        print("Connection failed")
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Unexpected disconnection")
+
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
-########################################
-broker_address="192.168.43.38"
-#broker_address="broker.hivemq.com"
-print("creating new instance")
-client = mqtt.Client("P1") #create new instance
-client.on_message=on_message #attach function to callback
-print("connecting to broker")
-client.connect(broker_address, port=1883) #connect to broker
-client.loop_start() #start the loop
-print("Subscribing to topic","house/bulbs/bulb1")
-client.subscribe("house/bulbs/bulb1")
-print("Publishing message to topic","house/bulbs/bulb1")
-client.publish("house/bulbs/bulb1","OFF")
-time.sleep(4) # wait
-client.loop_stop() #stop the loop
+    print("Message received:", message.payload.decode("utf-8"))
+    print("Message topic:", message.topic)
+    print("Message QoS:", message.qos)
+    print("Message retain flag:", message.retain)
+
+def on_publish(client, userdata, mid):
+    print("Message published")
+
+broker_address = "192.168.43.38"
+client = mqtt.Client("P1")
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+client.on_publish = on_publish
+
+print("Creating new instance")
+client.connect(broker_address, port=1883)
+
+client.loop_start()
+
+print("Publishing message to topic: house/bulbs/bulb1")
+client.publish("house/bulbs/bulb1", "OFF")
+
+time.sleep(4)
+
+client.loop_stop()
+client.disconnect()
