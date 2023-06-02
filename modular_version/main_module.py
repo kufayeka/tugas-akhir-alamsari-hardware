72cbecd75818775a894c17a5eeaeeb93f3a683bc
kufayeka <yayankufa18@gmail.com>
2023-06-02 12:06:49 +0700

    progress report: idk man... i just don't know

--- modular_version/main_module.py
+++ modular_version/main_module.py
@@ -1,17 +1,18 @@
 import datetime
 import time
+import threading
+import multiprocessing
+import RPi.GPIO as GPIO
 from utils import climate_sensors
 from utils import pid_calculator
 
 sensor1_humidity = 0
 PID_kp, PID_ki, PID_kd = 0.5, 0.05, 0.01
-PID_set_point = 90
+PID_set_point = 80
 PID_output = 0
 PWM_enabled = False
 PWM_high_time = 1   # Dynamic interval for relay_on()
 PWM_low_time = 2    # Dynamic interval for relay_off()
-on = 10
-off = 5
 min_interval, max_interval, swap_intervals = 0, 5, False
 
 def read_sensor(ts):
@@ -47,32 +48,78 @@ def relay_on(ts):
 def relay_off(ts):
     print("RELAY OFF", ts)
 
-if __name__ == '__main__':
-    sensor_interval = 1     # 1 second interval for read_sensor()
-    data_interval = 10      # 10 second interval for record_data()
-
+def sensor_work():
+    sensor_interval = 1  # 1 second interval for read_sensor()
+    data_interval = 10   # 10 second interval for record_data()
     sensor_last_time = time.time()
     data_last_time = time.time()
+    global PWM_high_time, PWM_low_time, PWM_enabled, PID_output
+
+    while True:
+        timestamp = datetime.datetime.now().strftime('%H:%M:%S.%f')[:-3]  # Get timestamp with milliseconds (removing the last 3 digits)
+
+        # Read the sensor data every 1 second
+        if time.time() - sensor_last_time >= sensor_interval:
+            read_sensor(timestamp)
+            sensor_last_time = time.time()
+
+        # Record the data every 10 seconds
+        if time.time() - data_last_time >= data_interval:
+            record_data(timestamp)
+            data_last_time = time.time()
+
+        # Continuously calculate PID
+        PWM_high_time, PWM_low_time, PWM_enabled, PID_output = pid_calculator.calculate_pid_output(PID_kp, PID_ki, PID_kd, PID_set_point, sensor1_humidity, min_interval, max_interval, swap_intervals)
+
+        time.sleep(0.1)  # Add a small delay to reduce CPU usage
+
+def relay_work():
+    while True:
+        timestamp = datetime.datetime.now().strftime('%H:%M:%S.%f')[:-3]  # Get timestamp with milliseconds (removing the last 3 digits)
+
+        # Turn on the relay
+        relay_on(timestamp)
+        time.sleep(1)
+
+        # Turn off the relay
+        relay_off(timestamp)
+        time.sleep(2)
+
+if __name__ == '__main__':
+    sensor_thread = threading.Thread(target=sensor_work)
+    relay_thread = threading.Thread(target=relay_work)
+    sensor_process = multiprocessing.Process(target=sensor_work)
+    relay_process = multiprocessing.Process(target=relay_work)
+
+    # setup GPIO
+    relayPin = 12
+    fanPin = 18	
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(relayPin, GPIO.OUT)
+    GPIO.setup(fanPin, GPIO.OUT)
 
     try:
-        while True:
-            timestamp = datetime.datetime.now().strftime('%H:%M:%S')
-            # Read the sensor data every 1 second
-            if time.time() - sensor_last_time >= sensor_interval:
-                read_sensor(timestamp)
-                sensor_last_time = time.time()
+        print("STARTING...\n\n")
+        time.sleep(1)
 
-            # Record the data every 10 seconds
-            if time.time() - data_last_time >= data_interval:
-                record_data(timestamp)
-                data_last_time = time.time()
-                
-            # Continuously calculate PID
-            PWM_high_time, PWM_low_time, PWM_enabled, PID_output = pid_calculator.calculate_pid_output(PID_kp, PID_ki, PID_kd, PID_set_point, sensor1_humidity, min_interval, max_interval, swap_intervals)
+        #sensor_thread.start()
+        #relay_thread.start()
+
+        sensor_process.start()
+        relay_process.start()
+
+        sensor_process.join()
+        relay_process.join()
 
     except KeyboardInterrupt:
-        print("\n\nKeyboardInterrupt detected. Exiting...")
+        print("\n\nKeyboard Interrupt Detected...")
 
     finally:
-        # Cleanup code, if any
-        pass
+        sensor_process.kill()
+        relay_process.kill()
+
+        GPIO.cleanup()
+        print("gpio cleaned up!")
+        print("excel data saved!")
+        print("TERMINATED!")
